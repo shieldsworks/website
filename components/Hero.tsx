8d91c2109a24dd98f555aef30f3ec272f14d8fdb
Casey Shields <casey@shieldsworks.com>
2024-10-11 20:07:00 -0700

    init

---
+++ components/Hero.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Component() {
+  return (
+    <div className="relative w-full overflow-hidden">
+      <h1 className="text-4xl font-bold text-center text-black mt-8">Casey Shields</h1>
+    </div>
+  );
+}
